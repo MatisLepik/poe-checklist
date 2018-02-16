946594e50ba557830f07da2b089e5512d0e1b4da
Matis Lepik <matis.lepik@gmail.com>
2018-02-17 01:33:11 +0200

    Import divination card data

---
+++ src/data/MAP_DIVINATION_CARDS.js
@@ -0,0 +1,516 @@
+export default {
+  "Arsenal Map": [
+    "Abandoned Wealth",
+    "Jack in the Box",
+    "The Saint's Treasure",
+    "The Warlord",
+    "Three Faces in the Dark"
+  ],
+  "Mao Kun": [
+    "Abandoned Wealth"
+  ],
+  "Vault Map": [
+    "Abandoned Wealth",
+    "The Hoarder",
+    "The Opulent"
+  ],
+  "Alleyways Map": [
+    "Assassin's Favour",
+    "The Blazing Fire"
+  ],
+  "Arcade Map": [
+    "Assassin's Favour",
+    "The Saint's Treasure"
+  ],
+  "Bazaar Map": [
+    "Assassin's Favour",
+    "The Saint's Treasure"
+  ],
+  "Park Map": [
+    "Assassin's Favour"
+  ],
+  "Relic Chambers Map": [
+    "Blind Venture",
+    "Might is Right",
+    "The Opulent",
+    "The Polymath"
+  ],
+  "Ivory Temple Map": [
+    "Boundless Realms",
+    "Dialla's Subjugation",
+    "The Opulent",
+    "The Twins"
+  ],
+  "Temple Map": [
+    "Boundless Realms",
+    "The Opulent"
+  ],
+  "Infested Valley Map": [
+    "Call to the First Ones",
+    "Struck by Lightning",
+    "The Coming Storm"
+  ],
+  "Poorjoy's Asylum": [
+    "Chaotic Disposition"
+  ],
+  "Bone Crypt Map": [
+    "Coveted Possession",
+    "Grave Knowledge",
+    "The Forsaken",
+    "The Standoff",
+    "The Summoner",
+    "The Wretched"
+  ],
+  "Bog Map": [
+    "Death"
+  ],
+  "Marshes Map": [
+    "Death",
+    "The Eye of the Dragon"
+  ],
+  "Mud Geyser Map": [
+    "Death"
+  ],
+  "Academy Map": [
+    "Destined to Crumble",
+    "The Scholar"
+  ],
+  "Museum Map": [
+    "Destined to Crumble",
+    "Shard of Fate",
+    "The Scholar"
+  ],
+  "Scriptorium Map": [
+    "Destined to Crumble",
+    "The Aesthete",
+    "The Scholar"
+  ],
+  "Colonnade Map": [
+    "Dying Anguish",
+    "Gemcutter's Promise",
+    "The Carrion Crow",
+    "The Coming Storm"
+  ],
+  "Ghetto Map": [
+    "Dying Anguish",
+    "Jack in the Box",
+    "The Gentleman",
+    "The Saint's Treasure"
+  ],
+  "Precinct Map": [
+    "Dying Anguish",
+    "The Saint's Treasure"
+  ],
+  "Desert Map": [
+    "Earth Drinker"
+  ],
+  "Dunes Map": [
+    "Earth Drinker"
+  ],
+  "Overgrown Ruin Map": [
+    "Emperor of Purity",
+    "Shard of Fate",
+    "The Dark Mage",
+    "The Summoner",
+    "The Surgeon",
+    "The Wretched"
+  ],
+  "Springs Map": [
+    "Emperor of Purity",
+    "Hunter's Resolve",
+    "The Betrayal"
+  ],
+  "Courtyard Map": [
+    "Emperor's Luck",
+    "Struck by Lightning",
+    "The Carrion Crow",
+    "Thunderous Skies"
+  ],
+  "Orchard Map": [
+    "Emperor's Luck"
+  ],
+  "Terrace Map": [
+    "Emperor's Luck",
+    "The Porcupine"
+  ],
+  "Crystal Ore Map": [
+    "Gift of the Gemling Queen",
+    "Volatile Power"
+  ],
+  "Cursed Crypt Map": [
+    "Grave Knowledge",
+    "The Summoner"
+  ],
+  "Beach Map": [
+    "Hope"
+  ],
+  "Lookout Map": [
+    "Hope"
+  ],
+  "Channel Map": [
+    "Humility"
+  ],
+  "Waterways Map": [
+    "Humility"
+  ],
+  "Port Map": [
+    "Jack in the Box",
+    "Lucky Connections"
+  ],
+  "Promenade Map": [
+    "Jack in the Box",
+    "The Carrion Crow",
+    "The Traitor"
+  ],
+  "Maelström of Chaos": [
+    "Lantador's Lost Love",
+    "Lysah's Respite",
+    "The Lover",
+    "The Siren"
+  ],
+  "Lighthouse Map": [
+    "Lantador's Lost Love"
+  ],
+  "Mineral Pools Map": [
+    "Lantador's Lost Love",
+    "Lysah's Respite",
+    "The Lover",
+    "The Survivalist"
+  ],
+  "Reef Map": [
+    "Lantador's Lost Love"
+  ],
+  "Underground Sea Map": [
+    "Lantador's Lost Love",
+    "Lysah's Respite",
+    "The Lover",
+    "The Survivalist",
+    "Treasure Hunter"
+  ],
+  "Residence Map": [
+    "Light and Truth",
+    "The Dapper Prodigy",
+    "The Opulent"
+  ],
+  "Villa Map": [
+    "Light and Truth",
+    "The Opulent"
+  ],
+  "Barrows Map": [
+    "Loyalty",
+    "The Fox"
+  ],
+  "Grotto Map": [
+    "Loyalty",
+    "Rain Tempter",
+    "The Watcher"
+  ],
+  "Lair Map": [
+    "Loyalty",
+    "The Wolf's Shadow",
+    "The Wolven King's Bite"
+  ],
+  "Shipyard Map": [
+    "Lucky Connections"
+  ],
+  "Wharf Map": [
+    "Lucky Connections"
+  ],
+  "Ramparts Map": [
+    "Lucky Deck",
+    "The Carrion Crow",
+    "The Traitor"
+  ],
+  "Dark Forest Map": [
+    "Mawr Blaidd",
+    "The Dragon",
+    "The Wolven King's Bite"
+  ],
+  "Mausoleum Map": [
+    "Might is Right",
+    "No Traces"
+  ],
+  "Sepulchre Map": [
+    "Might is Right",
+    "No Traces"
+  ],
+  "Iceberg Map": [
+    "Mitts"
+  ],
+  "Summit Map": [
+    "Mitts",
+    "The Ruthless Ceinture",
+    "The Wolverine"
+  ],
+  "Caldera Map": [
+    "Pride Before the Fall",
+    "The Battle Born",
+    "The King's Heart"
+  ],
+  "Lava Lake Map": [
+    "Pride Before the Fall",
+    "The Wrath"
+  ],
+  "Volcano Map": [
+    "Pride Before the Fall",
+    "The Battle Born",
+    "The King's Heart"
+  ],
+  "Shore Map": [
+    "Prosperity"
+  ],
+  "Castle Ruins Map": [
+    "Rain of Chaos",
+    "The Mercenary",
+    "The Road to Power"
+  ],
+  "Factory Map": [
+    "Rats",
+    "Three Faces in the Dark"
+  ],
+  "Coves Map": [
+    "Scholar of the Seas"
+  ],
+  "Cemetery Map": [
+    "Struck by Lightning",
+    "The Union"
+  ],
+  "Graveyard Map": [
+    "Struck by Lightning",
+    "The Coming Storm",
+    "The Union"
+  ],
+  "Leyline Map": [
+    "Struck by Lightning"
+  ],
+  "Tower Map": [
+    "Struck by Lightning",
+    "The Aesthete",
+    "The Offering",
+    "The Warden"
+  ],
+  "Core Map": [
+    "The Aesthete"
+  ],
+  "Tribunal Map": [
+    "The Aesthete",
+    "The Offering",
+    "The Soul"
+  ],
+  "Burial Chambers Map": [
+    "The Betrayal",
+    "The Doctor",
+    "The Incantation"
+  ],
+  "Mesa Map": [
+    "The Betrayal"
+  ],
+  "Spider Forest Map": [
+    "The Betrayal",
+    "The Doctor",
+    "The Incantation"
+  ],
+  "Strand Map": [
+    "The Betrayal",
+    "Thunderous Skies"
+  ],
+  "Haunted Mansion Map": [
+    "The Blazing Fire"
+  ],
+  "Siege Map": [
+    "The Blazing Fire"
+  ],
+  "Ancient City Map": [
+    "The Catalyst"
+  ],
+  "Maze Map": [
+    "The Catalyst",
+    "The Inventor"
+  ],
+  "Vaal Pyramid Map": [
+    "The Catalyst",
+    "The Inventor",
+    "The Spoiled Prince"
+  ],
+  "Vaal Temple Map": [
+    "The Catalyst"
+  ],
+  "Cage Map": [
+    "The Chains that Bind",
+    "The Warden"
+  ],
+  "Lava Chamber Map": [
+    "The Chains that Bind",
+    "The Standoff"
+  ],
+  "Pen Map": [
+    "The Chains that Bind",
+    "The Warden"
+  ],
+  "Pit Map": [
+    "The Chains that Bind",
+    "The King's Blade"
+  ],
+  "Whakawairua Tuahu": [
+    "The Doppelganger"
+  ],
+  "Excavation Map": [
+    "The Endurance"
+  ],
+  "Geode Map": [
+    "The Endurance",
+    "Volatile Power"
+  ],
+  "Thicket Map": [
+    "The Explorer"
+  ],
+  "Sulphur Vents Map": [
+    "The Eye of the Dragon"
+  ],
+  "Lair of the Hydra Map": [
+    "The Feast"
+  ],
+  "Toxic Sewer Map": [
+    "The Feast",
+    "The Throne"
+  ],
+  "Waste Pool Map": [
+    "The Feast"
+  ],
+  "The Putrid Cloister": [
+    "The Fiend"
+  ],
+  "Shrine Map": [
+    "The Fiend",
+    "The Lunaris Priestess"
+  ],
+  "Jungle Valley Map": [
+    "The Flora's Gift",
+    "The Wind"
+  ],
+  "Tropical Island Map": [
+    "The Flora's Gift"
+  ],
+  "Sunken City Map": [
+    "The Fox"
+  ],
+  "Arena Map": [
+    "The Gladiator"
+  ],
+  "Colosseum Map": [
+    "The Gladiator",
+    "The Visionary"
+  ],
+  "Acid Lakes Map": [
+    "The Inoculated"
+  ],
+  "Primordial Pool Map": [
+    "The Inoculated"
+  ],
+  "Gorge Map": [
+    "The Lion",
+    "The Wolverine"
+  ],
+  "Oba's Cursed Trove": [
+    "The One With All"
+  ],
+  "Chateau Map": [
+    "The Opulent"
+  ],
+  "Conservatory Map": [
+    "The Opulent"
+  ],
+  "Crimson Temple Map": [
+    "The Opulent"
+  ],
+  "Laboratory Map": [
+    "The Opulent"
+  ],
+  "Gardens Map": [
+    "The Porcupine"
+  ],
+  "Plaza Map": [
+    "The Porcupine"
+  ],
+  "Vaults of Atziri": [
+    "The Queen"
+  ],
+  "Arid Lake Map": [
+    "The Rabid Rhoa"
+  ],
+  "Dig Map": [
+    "The Ruthless Ceinture"
+  ],
+  "Maze of the Minotaur Map": [
+    "The Ruthless Ceinture"
+  ],
+  "Ashen Wood Map": [
+    "The Scarred Meadow"
+  ],
+  "Peninsula Map": [
+    "The Scarred Meadow"
+  ],
+  "Underground River Map": [
+    "The Scavenger",
+    "The Wolf's Shadow"
+  ],
+  "Overgrown Shrine Map": [
+    "The Soul",
+    "The Surgeon"
+  ],
+  "Atoll Map": [
+    "The Spark and the Flame",
+    "The Wind"
+  ],
+  "Forge of the Phoenix Map": [
+    "The Standoff",
+    "The Wretched"
+  ],
+  "Dungeon Map": [
+    "The Summoner",
+    "The Warden",
+    "The Wretched"
+  ],
+  "Malformation Map": [
+    "The Surveyor"
+  ],
+  "Moon Temple Map": [
+    "The Twins"
+  ],
+  "Coral Ruins Map": [
+    "The Vast"
+  ],
+  "Arachnid Nest Map": [
+    "The Web"
+  ],
+  "Arachnid Tomb Map": [
+    "The Web"
+  ],
+  "Spider Lair Map": [
+    "The Web"
+  ],
+  "Caer Blaidd, Wolfpack's Den": [
+    "The Wolf's Shadow"
+  ],
+  "Belfry Map": [
+    "The Wrath"
+  ],
+  "Cells Map": [
+    "The Wretched"
+  ],
+  "The Coward's Trial": [
+    "The Wretched"
+  ],
+  "Necropolis Map": [
+    "The Wretched"
+  ],
+  "Acton's Nightmare": [
+    "The Wretched"
+  ],
+  "Torture Chamber Map": [
+    "The Wretched"
+  ],
+  "Fields Map": [
+    "Tranquillity"
+  ]
+}; 
