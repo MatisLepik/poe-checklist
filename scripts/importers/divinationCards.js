57a671dc833a294bcdb51966844e8f315aa710f7
Matis Lepik <matis.lepik@gmail.com>
2018-02-17 01:32:58 +0200

    Add finished divination card importer

--- scripts/importers/divinationCards.js
+++ scripts/importers/divinationCards.js
@@ -1,15 +1,81 @@
+/**
+ * This script does the following:
+ * - Scrape divination card data from POE wiki
+ * - For each card, find all maps in the dropAreas string that end with (War for the Atlas) (case insensitive)
+ * - Output an object where keys are map names, and values are arrays of div cards.
+ */
+
 const importer = require('./wikiImporter');
+const fs = require('fs');
+const path = require('path');
 
 console.log('Importing...');
+
+const MAP_REGEX = /\[\[([^\]]+) \(war for the atlas\)\]\]/gi;
+const FILE_NAME = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  'src',
+  'data',
+  'MAP_DIVINATION_CARDS.js'
+);
 
 importer
   .getData({
     limit: 500,
     tables: 'items',
-    fields: 'name,drop_areas_html',
+    fields: 'name,drop_areas_html=dropAreas',
     where: 'class="Divination Card"',
     group_by: 'items._pageName',
   })
-  .then(data => {
-    console.log('data', data);
+  .then(findMapsInDivinationCardArray)
+  .then(sortByMaps)
+  .then(writeToDisc)
+  .then(console.log)
+  .catch(console.error);
+
+/**
+ * Replaces dropAreas in the div cards array with maps that drop them in the current version of the game
+ */
+function findMapsInDivinationCardArray(cards) {
+  return cards.map(card => {
+    const maps = [];
+    let match;
+
+    // eslint-disable-next-line no-cond-assign
+    while ((match = MAP_REGEX.exec(card.title.dropAreas)) !== null) {
+      maps.push(match[1]); // 1st capture group
+    }
+
+    return {
+      name: card.title.name,
+      maps,
+    };
   });
+}
+
+/**
+ * Takes an array of divination cards, and returns a hashmap of maps
+ */
+function sortByMaps(cards) {
+  const output = {};
+
+  cards.forEach(card => {
+    card.maps.forEach(map => {
+      if (!output[map]) {
+        output[map] = [];
+      }
+
+      output[map].push(card.name);
+    });
+  });
+
+  return output;
+}
+
+function writeToDisc(data) {
+  const fileContents = `export default ${JSON.stringify(data, null, 2)}; \n`;
+  fs.writeFileSync(FILE_NAME, fileContents);
+  return data;
+}
